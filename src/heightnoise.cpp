4b147e2e3e20117e4f97db29f95a1133220fda1a
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2020-02-21 15:45:44 +0100

    Doc

--- src/heightnoise.cpp
+++ src/heightnoise.cpp
@@ -1,6 +1,7 @@
 #include "heightnoise.h"
 
 #include <noise/noise.h>
+#include <math.h>
 
 using namespace noise;
 using namespace Eigen;
@@ -20,5 +21,5 @@ float HeightNoise::getValue(const Vector3f &keygen)
     module::Perlin myModule;
     myModule.SetFrequency(1.f);
     double height = myModule.GetValue(keygen.x(), keygen.y(), keygen.z());
-    return height;
+    return std::max(-1.0, std::min(1.0, height));
 }
--- src/heightnoise.h
+++ src/heightnoise.h
@@ -3,12 +3,20 @@
 
 #include <Eigen/Geometry>
 
+/**
+ * @brief The HeightNoise class generate noise.
+ */
 class HeightNoise
 {
 public:
     HeightNoise();
     ~HeightNoise();
-
+    /**
+     * @brief getValue get the value noise.
+     * @param keygen THe key of the generation.
+     * @return the value noise in float associated to the key.
+     * Domain between [-1, 1]
+     */
     float getValue(const Eigen::Vector3f &keygen);
 };
 
