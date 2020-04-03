ae0cd1ecb1a53ed1bba61c139f657f16309d63da
Marc Cerutti <marc.cerutti@etu.u-bordeaux.fr>
2020-04-03 16:03:57 +0200

    Test_results_files

--- src/tests/test_time_generation.cpp
+++ src/tests/test_time_generation.cpp
@@ -10,6 +10,7 @@ TEST(TimeGeneration, limits_subdivision) {
 
     int sub = 0;
     double duration = 0;
+    int nb_vertices = 0;
     while(duration < AIM_MAX_TIME_MS)
     {
         sub ++;
@@ -21,6 +22,7 @@ TEST(TimeGeneration, limits_subdivision) {
         Editor* ed = new NoisyHeight_Editor(shape);
         ed->edit();
         EXPECT_NO_FATAL_FAILURE();
+        nb_vertices = shape->getVertices()->_positions.size();
         delete ed;
         delete shape;
 
@@ -30,6 +32,7 @@ TEST(TimeGeneration, limits_subdivision) {
     }
 
     std::cout<<"Maximum subdivision with NoisyHeight_Editor : "<< sub<<" for "<< (duration) <<" ms.\n";
+    std::cout<<"Number of vertices : "<< nb_vertices<<" \n";
 }
 
 //Graphics TODO
